ceb6f17e5f511507f6bb82c54ffdfe5deeae4693
Yomi <modamorioluwayomi@gmail.com>
2020-09-15 00:10:28 +0100

    master

---
+++ adopt-pets/src/views/Cats.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Cats.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ adopt-pets/src/views/Dogs.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Dogs.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
